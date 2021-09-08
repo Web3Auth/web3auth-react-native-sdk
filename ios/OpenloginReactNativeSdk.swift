ee0b456834f019203992ccf10902da2912070972
michaellee8 <ckmichael8@gmail.com>
2021-09-08 10:23:36 +0800

    feat: add ios bridge

--- ios/OpenloginReactNativeSdk.swift
+++ ios/OpenloginReactNativeSdk.swift
@@ -1,8 +1,56 @@
-@objc(OpenloginReactNativeSdk)
-class OpenloginReactNativeSdk: NSObject {
+import OpenLogin
 
-    @objc(multiply:withB:withResolver:withRejecter:)
-    func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-        resolve(a*b)
+let OpenloginAuthStateChangedEvent = "OpenloginAuthStateChangedEvent"
+
+@objc(OpenloginReactNativeSdk)
+class OpenloginReactNativeSdk: RCTEventEmitter {
+    
+    private var webauth: WebAuth
+
+
+    
+    @objc(init:withResolver:withRejecter:)
+    func `init`(params: [String:String], resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+        guard
+            let clientId = params["clientId"] as? String,
+            let networkString = params["network"] as? String,
+            let network = Network(rawValue: networkString)
+        else {
+            reject("ArgumentError", "invalid clientId or network", nil)
+            return
+        }
+        webauth = OpenLogin.webAuth(clientId, network)
+        resolve(nil)
+    }
+    
+    @objc(login:withResolver:withRejecter:)
+    func login(params: [String:String], resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+        webauth.start {
+            switch $0 {
+            case .success(let result):
+                let m: [String: Any] = [
+                    "privKey": result.privKey,
+                    "userInfo": [
+                        "name": result.userInfo.name,
+                        "profileImage": result.userInfo.profileImage,
+                        "typeOfLogin": result.userInfo.typeOfLogin
+                    ]
+                ]
+                self.sendEvent(withName: OpenloginAuthStateChangedEvent, body: m)
+                resolve(nil)
+            case .failure(let error):
+                reject("LoginError", "Error occured during login with openlogin-swift-sdk", error)
+            }
+        }
+    }
+    
+    @objc(logout:withResolver:withRejecter:)
+    func logout(params: [String:String], resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+        webauth.singOut()
+        resolve(nil)
+    }
+    
+    @objc open override func supportedEvents() -> [String]{
+        return [OpenloginReactNativeSdk]
     }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,62 @@
-import { NativeModules } from 'react-native';
+import { NativeModules, NativeEventEmitter } from 'react-native';
+
+export interface AuthState {
+  privKey: string;
+  oAuthPrivateKey?: string;
+  tKey?: string;
+  walletKey?: string;
+}
 
 type OpenloginReactNativeSdkType = {
-  multiply(a: number, b: number): Promise<number>;
+  init(params: {
+    clientId: string;
+    network: OpenloginNetwork;
+    redirectUrl: string;
+  }): Promise<void>;
+  login(params: { provider: LoginProvider }): Promise<void>;
+  logout(params: {}): Promise<void>;
+  getState(): Promise<AuthState>;
 };
+
+const OpenloginAuthStateChangedEvent = 'OpenloginAuthStateChangedEvent';
+
+export enum LoginProvider {
+  GOOGLE = 'google',
+  FACEBOOK = 'facebook',
+  REDDIT = 'reddit',
+  DISCORD = 'discord',
+  TWITCH = 'twitch',
+  APPLE = 'apple',
+  LINE = 'line',
+  GITHUB = 'github',
+  KAKAO = 'kakao',
+  LINKEDIN = 'linkedin',
+  TWITTER = 'twitter',
+  WEIBO = 'weibo',
+  WECHAT = 'wechat',
+  EMAIL_PASSWORDLESS = 'email_passwordless',
+  WEBAUTHN = 'webauthn',
+  JWT = 'jwt',
+}
+
+export enum OpenloginNetwork {
+  MAINNET = 'mainnet',
+  TESTNET = 'testnet',
+  DEVELOPMENT = 'development',
+}
 
 const { OpenloginReactNativeSdk } = NativeModules;
 
-export default OpenloginReactNativeSdk as OpenloginReactNativeSdkType;
+const eventEmitter = new NativeEventEmitter(OpenloginReactNativeSdk);
+
+const extension = {
+  addOpenloginAuthStateChangedEventListener: (
+    listener: (state: AuthState) => void
+  ) => {
+    eventEmitter.addListener(OpenloginAuthStateChangedEvent, listener);
+  },
+};
+
+const sdk = OpenloginReactNativeSdk as OpenloginReactNativeSdkType;
+
+export default { ...sdk, ...extension };
