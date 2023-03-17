48d180a83d263d98649ef3440655edfcf7fd17b5
Archit <arch1995@gmail.com>
2023-03-17 13:04:17 +0530

    changes

--- rn-bare-example/metro.config.js
+++ rn-bare-example/metro.config.js
@@ -1,17 +1,32 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
+const {getDefaultConfig} = require('metro-config');
+const nodeLibs = require('node-libs-react-native');
 
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
+module.exports = (async () => {
+  const {
+    resolver: {sourceExts, assetExts},
+  } = await getDefaultConfig();
+
+  const defaultSourceExts = [...sourceExts, 'svg', 'mjs', 'cjs'];
+
+  return {
+    resolver: {
+      extraNodeModules: {
+        ...nodeLibs,
       },
-    }),
-  },
-};
+
+      assetExts: assetExts.filter(ext => ext !== 'svg'),
+
+      sourceExts: process.env.TEST_REACT_NATIVE
+        ? ['e2e.js'].concat(defaultSourceExts)
+        : defaultSourceExts,
+    },
+    transformer: {
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: true,
+        },
+      }),
+    },
+  };
+})();
