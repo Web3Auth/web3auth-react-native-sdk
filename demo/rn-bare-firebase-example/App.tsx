f359135e0d11d96cd8035ad00271d411fcad9997
Yashovardhan Agrawal <21066442+yashovardhan@users.noreply.github.com>
2025-12-09 19:29:30 +0530

    Add other custom auth examples

--- demo/rn-bare-firebase-example/App.tsx
+++ demo/rn-bare-firebase-example/App.tsx
@@ -1,36 +1,20 @@
 import "@ethersproject/shims";
 
-// IMP START - Quick Start
+import firebaseAuth from "@react-native-firebase/auth";
 import * as WebBrowser from "@toruslabs/react-native-web-browser";
-import {
-  AccountAbstractionProvider,
-  BiconomySmartAccount,
-  ISmartAccount,
-  KernelSmartAccount,
-  SafeSmartAccount,
-  TrustSmartAccount,
-} from "@web3auth/account-abstraction-provider";
 import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider";
-import Web3Auth, { AUTH_CONNECTION, CHAIN_NAMESPACES, WEB3AUTH_NETWORK } from "@web3auth/react-native-sdk";
+import Web3Auth, { AUTH_CONNECTION, ChainNamespace, WEB3AUTH_NETWORK } from "@web3auth/react-native-sdk";
 import { ethers } from "ethers";
 import React, { useEffect, useState } from "react";
-import { Button, Dimensions, ScrollView, StyleSheet, Switch, Text, TextInput, View } from "react-native";
+import { Button, Dimensions, ScrollView, StyleSheet, Text, View } from "react-native";
 import EncryptedStorage from "react-native-encrypted-storage";
-import { MMKVLoader, useMMKVStorage } from "react-native-mmkv-storage";
-// IMP END - Quick Start
 
-const scheme = "web3authrnexample"; // Or your desired app redirection scheme
-// IMP START - Whitelist bundle ID
+const scheme = "web3authrnbarefirebase"; // Or your desired app redirection scheme
 const redirectUrl = `${scheme}://auth`;
-// IMP END - Whitelist bundle ID
+const clientId = "BPi5PB_UiIZ-cPz1GtV5i1I2iOSOHuimiXBI0e-Oe_u6X3oVAbCiAZOTEBtTXw4tsluTITPqA8zMsfxIKMjiqNQ";
 
-// IMP START - Dashboard Registration
-const clientId = "BCfIbiMcEwBkmyNxwn-DcYIfUU4QrpQgyOZZTNi5f_ygWMS1g_dNcuxylwDkIbVNhDtn7dAs-aMUhX0dtAYhvWk"; // get from https://dashboard.web3auth.io
-// IMP END - Dashboard Registration
-
-// IMP START - SDK Initialization
 const chainConfig = {
-  chainNamespace: CHAIN_NAMESPACES.EIP155,
+  chainNamespace: ChainNamespace.EIP155,
   chainId: "0xaa36a7",
   rpcTarget: `https://api.web3auth.io/infura-service/v1/0xaa36a7/${clientId}`,
   // Avoid using public rpcTarget in production.
@@ -49,95 +33,36 @@ const privateKeyProvider = new EthereumPrivateKeyProvider({
   },
 });
 
-const PIMLICO_API_KEY = "pim_WDBELWbZeo9guUAr7HNFaF";
-
-export const getDefaultBundlerUrl = (chainId: string): string => {
-  return `https://api.pimlico.io/v2/${Number(chainId)}/rpc?apikey=${PIMLICO_API_KEY}`;
-};
-
-export type SmartAccountType = "safe" | "kernel" | "biconomy" | "trust";
-
-export type AccountAbstractionConfig = {
-  bundlerUrl?: string;
-  paymasterUrl?: string;
-  smartAccountType?: SmartAccountType;
-};
-
-const AAConfig: AccountAbstractionConfig = {
-  // bundlerUrl: "https://bundler.safe.global",
-  // paymasterUrl: "https://paymaster.safe.global",
-  smartAccountType: "safe",
-};
-
-const storage = new MMKVLoader().initialize();
-// IMP END - SDK Initialization
+async function signInWithEmailPassword() {
+  try {
+    const res = await firebaseAuth().signInAnonymously();
+    return res;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 export default function App() {
   const [web3auth, setWeb3auth] = useState<Web3Auth | null>(null);
   const [loggedIn, setLoggedIn] = useState(false);
   const [provider, setProvider] = useState<any>(null);
   const [console, setConsole] = useState<string>("");
-  const [email, setEmail] = useState<string>("");
-  const [useAccountAbstraction, setUseAccountAbstraction] = useMMKVStorage<boolean>("useAccountAbstraction", storage, false);
-
-  const toggleAccountAbstraction = () => {
-    setUseAccountAbstraction((prevState) => !prevState);
-  };
 
   useEffect(() => {
     const init = async () => {
-      // setup aa provider
-      let aaProvider: AccountAbstractionProvider | undefined;
-      if (useAccountAbstraction) {
-        const { bundlerUrl, paymasterUrl, smartAccountType } = AAConfig;
-
-        let smartAccountInit: ISmartAccount;
-        switch (smartAccountType) {
-          case "biconomy":
-            smartAccountInit = new BiconomySmartAccount();
-            break;
-          case "kernel":
-            smartAccountInit = new KernelSmartAccount();
-            break;
-          case "trust":
-            smartAccountInit = new TrustSmartAccount();
-            break;
-          // case "light":
-          //   smartAccountInit = new LightSmartAccount();
-          //   break;
-          // case "simple":
-          //   smartAccountInit = new SimpleSmartAccount();
-          //   break;
-          case "safe":
-          default:
-            smartAccountInit = new SafeSmartAccount();
-            break;
-        }
-
-        aaProvider = new AccountAbstractionProvider({
-          config: {
-            chainConfig,
-            bundlerConfig: {
-              url: bundlerUrl ?? getDefaultBundlerUrl(chainConfig.chainId),
-            },
-            paymasterConfig: paymasterUrl
-              ? {
-                  url: paymasterUrl,
-                }
-              : undefined,
-            smartAccountInit,
-          },
-        });
-      }
-
       const web3auth = new Web3Auth(WebBrowser, EncryptedStorage, {
         clientId,
-        // IMP START - Whitelist bundle ID
         redirectUrl,
-        // IMP END - Whitelist bundle ID
         network: WEB3AUTH_NETWORK.SAPPHIRE_MAINNET, // or other networks
         privateKeyProvider,
-        accountAbstractionProvider: aaProvider,
+        loginConfig: {
+          jwt: {
+            name: "Web3Auth-Auth0-JWT",
+            verifier: "w3a-firebase-demo",
+            typeOfLogin: "jwt",
+            clientId,
+          },
+        },
       });
       setWeb3auth(web3auth);
 
@@ -151,7 +76,7 @@ export default function App() {
       }
     };
     init();
-  }, [useAccountAbstraction]);
+  }, []);
 
   const login = async () => {
     try {
@@ -159,17 +84,21 @@ export default function App() {
         setConsole("Web3auth not initialized");
         return;
       }
-      if (!email) {
-        setConsole("Enter email first");
-        return;
-      }
 
       setConsole("Logging in");
+      const loginRes = await signInWithEmailPassword();
+      uiConsole("Login success", loginRes);
+      const idToken = await loginRes?.user.getIdToken(true);
+      uiConsole("idToken", idToken);
+
       // IMP START - Login
       await web3auth.connectTo({
-        authConnection: AUTH_CONNECTION.EMAIL_PASSWORDLESS,
+        authConnection: AUTH_CONNECTION.CUSTOM,
+        authConnectionId: "w3a-firebase-demo",
+        idToken: idToken,
         extraLoginOptions: {
-          login_hint: email,
+          verifierIdField: "sub",
+          domain: "http://localhost:3000",
         },
       });
       uiConsole(web3auth.userInfo);
@@ -296,7 +225,7 @@ export default function App() {
       const address: string = await getAccounts();
 
       const params = ["Hello World", address];
-      const res = await web3auth.request("personal_sign", params);
+      const res = await web3auth.request(chainConfig, "personal_sign", params);
       uiConsole(res);
     } catch (error) {
       uiConsole("Error in requestSignature:", error);
@@ -316,21 +245,8 @@ export default function App() {
   );
 
   const unloggedInView = (
-    <View style={styles.buttonAreaLogin}>
-      <View style={{ marginBottom: 20 }}>
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "space-between",
-          }}
-        >
-          <Text style={{ paddingRight: 6 }}>Use Account Abstraction:</Text>
-          <Switch onValueChange={toggleAccountAbstraction} value={useAccountAbstraction} />
-        </View>
-      </View>
-      <TextInput style={styles.inputEmail} placeholder="Enter email" onChangeText={setEmail} />
-      <Button title="Login with Web3Auth" onPress={login} />
+    <View style={styles.buttonArea}>
+      <Button title="Login with Web3Auth" onPress={() => login()} />
     </View>
   );
 
@@ -377,20 +293,5 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "space-around",
     paddingBottom: 30,
-  },
-  buttonAreaLogin: {
-    flex: 2,
-    alignItems: "center",
-    justifyContent: "center",
-    paddingBottom: 30,
-  },
-  inputEmail: {
-    height: 40,
-    width: 300,
-    borderColor: "gray",
-    borderWidth: 1,
-    padding: 10,
-    borderRadius: 5,
-    marginBottom: 20,
   },
 });
