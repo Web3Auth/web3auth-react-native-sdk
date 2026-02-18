1f49952c38761d48de8d9eb99c39aeac946fbe47
Tuna <tunguyenanh@tunatech.org>
2026-02-18 19:20:49 +0700

    feat: update rn bare aggregate verifier demo

--- demo/rn-bare-aggregate-verifier-example/index.js
+++ demo/rn-bare-aggregate-verifier-example/index.js
@@ -1,5 +1,9 @@
+// Web3Auth setup - must be imported first before any other imports
+import "@web3auth/react-native-sdk/setup";
+
 import { AppRegistry } from "react-native";
-import "./globals";
+
 import App from "./App";
 import { name as appName } from "./app.json";
+
 AppRegistry.registerComponent(appName, () => App);
