25605ef32911e4f8572818152c9a53ae489a30fa
Yashovardhan Agrawal <21066442+yashovardhan@users.noreply.github.com>
2026-04-22 14:57:28 +0700

    Fix demos

--- demo/rn-bare-aggregate-verifier-hooks-example/web3authConfig.ts
+++ demo/rn-bare-aggregate-verifier-hooks-example/web3authConfig.ts
@@ -2,7 +2,7 @@ import { WEB3AUTH_NETWORK } from "@web3auth/react-native-sdk";
 import { type Web3AuthContextConfig } from "@web3auth/react-native-sdk";
 
 // IMP START - Dashboard Registration
-const clientId = "BHgArYmWwSeq21czpcarYh0EVq2WWOzflX-NTK-tY1-1pauPzHKRRLgpABkmYiIV_og9jAvoIxQ8L3Smrwe04Lw"; // get from https://dashboard.web3auth.io
+const clientId = "BPi5PB_UiIZ-cPz1GtV5i1I2iOSOHuimiXBI0e-Oe_u6X3oVAbCiAZOTEBtTXw4tsluTITPqA8zMsfxIKMjiqNQ"; // get from https://dashboard.web3auth.io
 // IMP END - Dashboard Registration
 
 // IMP START - SDK Initialization
@@ -10,8 +10,7 @@ const web3AuthContextConfig: Web3AuthContextConfig = {
   web3AuthOptions: {
     clientId,
     redirectUrl: "rnbareaggverifhooksexample://auth",
-    network: WEB3AUTH_NETWORK.SAPPHIRE_DEVNET,
-    enableLogging: true,
+    network: WEB3AUTH_NETWORK.SAPPHIRE_MAINNET,
   },
 };
 // IMP END - SDK Initialization
